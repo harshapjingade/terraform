80400807ddd8216c13e467d35bafeeb2335d157b
harshapjingade <34507186+harshapjingade@users.noreply.github.com>
2020-12-01 20:08:20 +0530

    Create main.tf

---
+++ Terraform_Examples/Terraform_Pipeline/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-south-1"
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-05695932c5299858a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Created via terraform"
+  }
+}
