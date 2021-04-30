198a44f9b9a4af2ee113a263fe2cd82a36d60663
harshapjingade <34507186+harshapjingade@users.noreply.github.com>
2021-04-30 17:50:12 +0530

    Add files via upload

---
+++ Terraform_Examples/EC2_Creation/terraform.tfvars
@@ -0,0 +1,6 @@
+region            = "ap-south-1"
+ami               = "ami-05695932c5299858a"
+instance_type     = "t2.micro"
+vpc_cidr_block    = "10.0.0.0/16"
+subnet_cidr_block = "10.0.1.0/24"
+availability_zone = "ap-south-1a"
---
+++ Terraform_Examples/EC2_Creation/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
+variable "subnet_cidr_block" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
+
+
