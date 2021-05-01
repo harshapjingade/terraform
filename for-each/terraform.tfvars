5ba598946be1901d30d235a73046592889dbea51
harshapjingade <34507186+harshapjingade@users.noreply.github.com>
2021-05-01 20:12:24 +0530

    Add files via upload

---
+++ for-each/terraform.tfvars
@@ -0,0 +1,3 @@
+region  = "ap-south-1"
+profile = "terraform"
+ami     = "ami-05695932c5299858a"
---
+++ for-each/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type = string
+}
+
+variable "profile" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
